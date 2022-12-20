f84eecfaab219eeb85234cbc5533e55d13ae1d05
Maksym Rutkovskyi <maksym.rutkovskyi@glomex.com>
2022-12-20 09:36:31 +0200

    change interval

--- code.py
+++ code.py
@@ -6,21 +6,21 @@ GPIO.setwarnings(False)
 GPIO.setup(4,GPIO.OUT)
 GPIO.setup(17,GPIO.OUT)
 t = 1
+lowActiveTime = 0.17
+
+
 GPIO.output(4, GPIO.HIGH)
 GPIO.output(17, GPIO.HIGH)
 
 def xyak (p) :
-    GPIO.output(p, GPIO.HIGH)
-    time.sleep(0.1)
     GPIO.output(p, GPIO.LOW)
-    time.sleep(0.1)
+    time.sleep(0.17)
     GPIO.output(p, GPIO.HIGH)
     time.sleep(t)
 
 def selectNum(i) :
     for j in range(i) :
         xyak(17)
-    time.sleep(1)
 
 def selectFullNum(arr) :
     for i in arr :
@@ -34,10 +34,10 @@ def showPassPanel() :
 
 def moveToNext() :
     xyak(4)
-    time.sleep(2)
+    time.sleep(1)
             
 digits = list(range(0, 10))
-startWith = "0052"
+startWith = "0180"
 for pas in itertools.product(digits, repeat=4):
     if str(pas[0]) + str(pas[1]) + str(pas[2]) + str(pas[3]) >= startWith:
         print(pas)
